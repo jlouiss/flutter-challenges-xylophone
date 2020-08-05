4a3b827748e92307752919f1fec680e0b4836a95
Jean Louis Borba Rosa Salbego <info@jlouis.me>
2020-08-05 20:13:46 +0100

    :recycle: Refactor sound buttons

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,18 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   final player = AudioCache();
 
+  Expanded buildKey({Color color, String soundName}) {
+    return Expanded(
+      child: FlatButton(
+        child: null,
+        color: color,
+        onPressed: () {
+          player.play(soundName);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,69 +30,13 @@ class XylophoneApp extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Expanded(
-                  child: FlatButton(
-                    child: null,
-                    color: Colors.red,
-                    onPressed: () {
-                      player.play('note1.wav');
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    child: null,
-                    color: Colors.orange,
-                    onPressed: () {
-                      player.play('note2.wav');
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    child: null,
-                    color: Colors.yellow,
-                    onPressed: () {
-                      player.play('note3.wav');
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    child: null,
-                    color: Colors.green,
-                    onPressed: () {
-                      player.play('note4.wav');
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    child: null,
-                    color: Colors.blue,
-                    onPressed: () {
-                      player.play('note5.wav');
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    child: null,
-                    color: Colors.indigo,
-                    onPressed: () {
-                      player.play('note6.wav');
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    child: null,
-                    color: Colors.purple,
-                    onPressed: () {
-                      player.play('note7.wav');
-                    },
-                  ),
-                ),
+                buildKey(color: Colors.red, soundName: 'note1.wav'),
+                buildKey(color: Colors.orange, soundName: 'note2.wav'),
+                buildKey(color: Colors.yellow, soundName: 'note3.wav'),
+                buildKey(color: Colors.green, soundName: 'note4.wav'),
+                buildKey(color: Colors.blue, soundName: 'note5.wav'),
+                buildKey(color: Colors.indigo, soundName: 'note6.wav'),
+                buildKey(color: Colors.purple, soundName: 'note7.wav'),
               ],
             ),
           ),
