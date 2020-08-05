0d9260b9a843e0ce91214cafc547622bedf19222
Jean Louis Borba Rosa Salbego <info@jlouis.me>
2020-08-05 20:01:31 +0100

    :sparkles: Add sound buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,91 @@
-void main() {}
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: SafeArea(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    child: null,
+                    color: Colors.red,
+                    onPressed: () {
+                      player.play('note1.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    child: null,
+                    color: Colors.orange,
+                    onPressed: () {
+                      player.play('note2.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    child: null,
+                    color: Colors.yellow,
+                    onPressed: () {
+                      player.play('note3.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    child: null,
+                    color: Colors.green,
+                    onPressed: () {
+                      player.play('note4.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    child: null,
+                    color: Colors.blue,
+                    onPressed: () {
+                      player.play('note5.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    child: null,
+                    color: Colors.indigo,
+                    onPressed: () {
+                      player.play('note6.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    child: null,
+                    color: Colors.purple,
+                    onPressed: () {
+                      player.play('note7.wav');
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
